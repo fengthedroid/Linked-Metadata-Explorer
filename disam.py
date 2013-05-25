b108c2ff94c0858aad357ea47edf5c2faf64adc0
Feng Wu <fw1714@mun.ca>
2013-05-25 16:57:00 -0230

    read argument from command line

--- disam.py
+++ disam.py
@@ -6,7 +6,15 @@ Author  Feng Wu
 import sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-rawWord = 'blade runner'
+
+#Split words into a list
+rawList = sys.argv[1].split(' ')
+
+filterString = ''
+for i in range(1,len(rawList)):
+	filterString+='FILTER regex(?label, "['+rawList[i][0].upper()+\
+		rawList[i][0].lower()+']'+rawList[i][1:]+'").\n'
+	
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 queryStr = """
 select ?s ?label ?abstract where {
@@ -14,10 +22,11 @@ select ?s ?label ?abstract where {
        dbpedia-owl:abstract ?abstract.
     FILTER langMatches( lang(?label), "en" ).
     FILTER langMatches( lang(?abstract), "en" ).
-    ?label bif:contains "Blade" .
-FILTER (CONTAINS(?label, "Runner"))
-}
+    ?label bif:contains '"""+rawList[0].title()+"""'.
+	"""+filterString+"""
+} LIMIT 10
 """
+print (queryStr)
 sparql.setQuery(queryStr)
 
 sparql.setReturnFormat(JSON)
@@ -26,4 +35,4 @@ results = sparql.query().convert()
 for result in results["results"]["bindings"]:
     print (result["s"]["value"],'\n')
     print (result["label"]["value"],'\n')
-    print (result["abstract"]["value"],'\n')
+    print (result["abstract"]["value"][:800],'\n')
