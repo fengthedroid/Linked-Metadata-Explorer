1b0f07a0bad68fbbb3f2cc95800a6fd3c9268bb0
Feng Wu <fw1714@mun.ca>
2013-05-26 22:37:01 -0230

    refactored the first component

--- disam.py
+++ disam.py
@@ -1,64 +1,78 @@
 '''
-Ver     0.2
-Author  Feng Wu
+Ver:    0.2
+Author: Feng Wu
+Env:	Run on python 3.3
 '''
-
-import pprint
 import sys
 import rdflib
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-#Split words into a list
-rawList = sys.argv[1].split(' ')
+def disam(keywords):
 
-filterString = ''
-for i in range(1,len(rawList)):
-	filterString+='FILTER regex(?label, "['+rawList[i][0].upper()+\
-		rawList[i][0].lower()+']'+rawList[i][1:]+'").\n'
+	#build the query string
+	queryStr = queryKeyword(keywords)
+	print (queryStr)
 	
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-
-queryStr = """
-construct {
-?subj 	rdfs:label 	?label;
-		rdf:type  	?type;
-		rdfs:comment	?comment.
-}
-where {
-    ?subj 	rdfs:label 	?label;
-			rdf:type	?type;
-			rdfs:comment 	?comment.
-    FILTER langMatches( lang(?label), "en" ).
-    FILTER langMatches( lang(?comment), "en" ).
-    ?label bif:contains '"""+rawList[0].title()+"""'.
-	"""+filterString+"""
-}
-"""
-print (queryStr)
-
-sparql.setQuery(queryStr)
-
-sparql.setReturnFormat('rdf')
-
-try:
-	sys.stderr=''
-	results = sparql.query().convert()
-	sys.stderr=sys.__stderr__
-except:
-	print (traceback.format_exc())
-finally:
-	pass
+	#execute the query
+	results = execQuery(queryStr)	
+	#print("graph has %s statements." % len(results))
+	
+	#iterate through the results and put each topic into a list
+	topicList = []
+	index = 0
+	for subjUri in results.subjects(predicate=rdflib.namespace.RDFS.label):		
+		topic = rdflib.resource.Resource(results,subjUri)	
+		topicList.append(topic)
+		print("No."+index+"  Title:  ",topic.label())
+		print("Absract: ",topic.comment())		
+		for objType in topic.objects(rdflib.namespace.RDF.type):
+			print("\tThe type of the topic is",objType.qname())	
+		print("--------------\n")
+		index += 1
+		
 	
 
-for subjUri in results.subjects(predicate=rdflib.namespace.RDFS.label):
 	
-	#print (subjUri)
-	topic = rdflib.resource.Resource(results,subjUri)
+def queryKeyword(keywords):
+	
+	#Split words into a list
+	rawList = keywords.split(' ')
 
-	pprint.pprint(topic.label())
-	#pprint.pprint(topic.comment())
-	for objType in results.objects(subject=subjUri,predicate=rdflib.namespace.RDF.type):
-		print("\tThe type of the topic is",objType)
-	
-	
-print("graph has %s statements." % len(results))
+	#
+	filterString = ''
+	for i in range(1,len(rawList)):
+		filterString+='FILTER regex(?label, "['+rawList[i][0].upper()+\
+			rawList[i][0].lower()+']'+rawList[i][1:]+'").\n'
+		
+	queryStr = """
+	construct {
+	?subj 	rdfs:label 	?label;
+			rdf:type  	?type;
+			rdfs:comment	?comment.
+	}
+	where {
+		?subj 	rdfs:label 	?label;
+				rdf:type	?type;
+				rdfs:comment 	?comment.
+		FILTER langMatches( lang(?label), "en" ).
+		FILTER langMatches( lang(?comment), "en" ).
+		?label bif:contains '"""+rawList[0].title()+"""'.
+		"""+filterString+"""
+	}
+	"""
+	return queryStr
+
+def execQuery(queryStr):
+	sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+	sparql.setQuery(queryStr)
+	sparql.setReturnFormat('rdf')
+
+	try:
+		sys.stderr=''
+		results = sparql.query().convert()
+		sys.stderr=sys.__stderr__
+	except:
+		print (traceback.format_exc())
+		raise
+		
+	return results
---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Ver:		0.1
+Author:		Feng Wu
+Env:		Run on python 3.3
+'''
+import sys
+from disam import *
+
+chosenTopic = disam(sys.argv[1])
